b897c7359f1186114fc3df47803cfe7e8f00ac4a
savanna chiang <savannaac@gmail.com>
2021-06-29 21:24:19 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+    changeText()
+});
+
+function changeText() {
+    const textNode = document.getElementById("text")
+    textNode.innerHTML = "This is really cool!";
+    // OR (both work)
+    // document.getElementById("text").innerHTML = "This is really cool!"
+}
